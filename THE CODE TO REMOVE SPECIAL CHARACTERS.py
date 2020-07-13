dd07f19e359ed3e1f5e8922284ffd4d7eaf4a4f9
SevakSingh <68162450+SevakSingh@users.noreply.github.com>
2020-07-13 13:29:10 -0600

    THE CODE TO REMOVE SPECIAL CHARACTERS
    This is a tutorial to do remove special characters. It is a 6 step operation and it is simple for beginners. 
    
    
    Thank you

---
+++ THE CODE TO REMOVE SPECIAL CHARACTERS.py
@@ -0,0 +1,35 @@
+In this code, you will learn how to remove special characters.
+
+#1 == make the function
+
+      def remove_special_char(txt):
+      
+#2 == define the special characters
+
+      special = set("!@#$%^&\'*().[]{}<>+=~,`|?")
+      
+#3 == define the output
+
+      output = ' '
+      
+#4 == make the for loop
+      
+       for char in txt:
+    if char not in special:
+      output += char
+ 
+ #5 == return the output
+ 
+      return output
+ 
+ #6 == You are done!!!
+
+
+
+def remove_special_characters(txt):
+  special = set("!@#$%^&\'*().[]{}<>+=~,`|?")
+  output = ''
+  for char in txt:
+    if char not in special:
+      output += char
+  return output
